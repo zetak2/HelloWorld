5501128afc8372e1b7a4d0a5a4e013426321b25b
Jose Manuel Alvarez <jose.manuel.alvarez@ibm.com>
2018-08-01 09:10:36 -0500

    Carga inicial

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.GregorianCalendar;
+
+public class HelloWorld {
+	public GregorianCalendar calendario = new GregorianCalendar();	
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo!!");
+
+	}
+
+}
